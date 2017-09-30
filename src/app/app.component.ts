cfdd228cca37105225c80d2213c303dc1ce06bc4
admin <cvhungkk@gmail.com>
2017-09-30 09:43:49 +0700

    tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+    import { Component } from '@angular/core';
+    import {trigger, state, style, transition,animate,keyframes} from '@angular/animations';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+    @Component({
+      selector: 'app-root',
+      template: `
+      <p [@myAwesomeAnimation]='state' (click)="animateMe()">I will animate </p>'
+      `,
+      styles: [`
+        p {
+            width: 200px;
+            background: lightgray;
+            margin: 100px auto;
+            text-align: center;
+            padding: 20px;
+            font-size:1.5em;
+        }
+      `],
+      animations: [
+        trigger('myAwesomeAnimation',[
+
+         state('small', style({
+         transform: 'scale(1)',
+         })),
+
+         state('large', style({
+         transform: 'scale(1,2)',
+         })),
+
+         transition('small <=> large', animate('300ms ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: 1, transform: 'translateY(35px)', offset: 0.5}),
+          style({opacity: 1, transform: 'translateY(0)', offset: 1})
+         ]))),
+        ]),
+      ]
+    })
+    export class AppComponent {
+      title = 'app';
+
+      state: string = 'small'
+
+      animateMe() {
+        console.log("click animateMe")
+        this.state = (this.state == 'small' ? 'large' : 'small')
+        console.log(this.state)
+      }
+
+    }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
